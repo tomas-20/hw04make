ac7bdfd2feeffefefe1f541cc4744a603e7c3d49
tomas-20 <tacuna20@stuy.edu>
2021-10-04 21:17:00 -0400

    created euler.h

---
+++ euler.h
@@ -0,0 +1,9 @@
+int factorsum(int n, int f);
+int euler1(int n);
+int gcf(int a, int b);
+int lcm(int a, int b);
+int euler5(int n);
+int sum_of_squares(int n);
+int square(int n);
+int square_of_sum(int n);
+int euler6(int n);
