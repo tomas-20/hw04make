f041311bffe0aa3c558ef0aa16c97e7261e69a3c
tomas-20 <tacuna20@stuy.edu>
2021-10-04 21:27:47 -0400

    created main.c

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "euler.h"
+int main() {
+  printf("euler1(1000)=%d\n", euler1(1000));
+  printf("euler5(20)=%d\n", euler5(20));
+  printf("euler6(100)=%d\n", euler6(100));
+  return 0;
+}
